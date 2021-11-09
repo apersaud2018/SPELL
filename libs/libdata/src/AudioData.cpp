4acfcbd95f649e719e1a93cb332d5bcd01f4ba90
Nandith Narayan <nandith2ndemail@gmail.com>
2021-11-09 17:33:32 -0500

    Added reading of wav files

--- libs/libdata/src/AudioData.cpp
+++ libs/libdata/src/AudioData.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <fstream>
 
+
 void AudioData::readData(std::string path){
     header_t header;
     FILE *input_file;
@@ -14,8 +15,22 @@ void AudioData::readData(std::string path){
     
     AudioData::sample_rate = header.sample_rate;
     
-    std::cout << AudioData::sample_rate << "\n";
+    std::cout << "Sample Rate: " << AudioData::sample_rate << "Hz\n";
     
+    int num_samples = header.data_size;
+    int sample_size = header.sample_size_bits;
+    std::cout  << num_samples << " Samples\n";
+    std::cout  << sample_size << " Bits per Sample\n";
+    
+
+    
+    for(int i=0;i<num_samples;i++){
+        int sample = 0;
+        std::fread(&sample, sample_size/8, 1, input_file);
+        double noramlized_sample = (double)sample / (double)(1 << (sample_size-1));
+        noramlized_sample += 1.0;
+        data.push_back(noramlized_sample);
+    }
     
     
 }
