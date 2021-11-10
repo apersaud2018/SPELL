01ac2fb46d5f924241cdd9df94915128f502bc25
Nandith Narayan <nandith2ndemail@gmail.com>
2021-11-10 02:31:50 -0500

    Fixed Sliding window

--- src/ts/spell_en_US.ts
+++ src/ts/spell_en_US.ts
@@ -5,71 +5,85 @@
     <name>SPELLMainWindow</name>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="14"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="220"/>
         <source>SPELL - Singing Phoneme Ergonamic Labeler</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="23"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="223"/>
         <source>Files</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="39"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="224"/>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="46"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="225"/>
         <source>Remove</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="61"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="226"/>
         <source>Play</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="68"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="227"/>
         <source>Pause</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="81"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="228"/>
         <source>Zoom</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="88"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="229"/>
         <source>+</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="95"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="230"/>
         <source>-</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="162"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="231"/>
         <source>Auto Scale</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="173"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="232"/>
         <source>Insert Label</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="192"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="233"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="202"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="221"/>
         <source>New</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="207"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="222"/>
         <source>Open</source>
         <translation type="unfinished"></translation>
     </message>
