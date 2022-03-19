df435ba592ae7e8ced135e5d08d167e843676a14
Nandith-Narayan <nandith2ndemail@gmail.com>
2022-03-19 16:28:20 -0400

    added zoom slider

--- src/ts/spell_es_US.ts
+++ src/ts/spell_es_US.ts
@@ -5,85 +5,73 @@
     <name>SPELLMainWindow</name>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="14"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="213"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="209"/>
         <source>SPELL - Singing Phoneme Ergonamic Labeler</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="28"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="216"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="212"/>
         <source>Files</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="44"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="217"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="213"/>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="51"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="218"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="214"/>
         <source>Remove</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="66"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="219"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="215"/>
         <source>Play</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="73"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="220"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="216"/>
         <source>Pause</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../views/SPELLMainWindow.ui" line="86"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="221"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="217"/>
         <source>Zoom</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../views/SPELLMainWindow.ui" line="93"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="222"/>
-        <source>+</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../views/SPELLMainWindow.ui" line="100"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="223"/>
-        <source>-</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../views/SPELLMainWindow.ui" line="109"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="224"/>
+        <location filename="../views/SPELLMainWindow.ui" line="102"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="218"/>
         <source>Auto Scale</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../views/SPELLMainWindow.ui" line="174"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="225"/>
+        <location filename="../views/SPELLMainWindow.ui" line="167"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="219"/>
         <source>New Track</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../views/SPELLMainWindow.ui" line="195"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="226"/>
+        <location filename="../views/SPELLMainWindow.ui" line="188"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="220"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../views/SPELLMainWindow.ui" line="205"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="214"/>
+        <location filename="../views/SPELLMainWindow.ui" line="198"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="210"/>
         <source>New</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../views/SPELLMainWindow.ui" line="210"/>
-        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="215"/>
+        <location filename="../views/SPELLMainWindow.ui" line="203"/>
+        <location filename="../../build/spell_autogen/include/ui_SPELLMainWindow.h" line="211"/>
         <source>Open</source>
         <translation type="unfinished"></translation>
     </message>
--- src/views/SPELLMainWindow.h
+++ src/views/SPELLMainWindow.h
@@ -35,6 +35,7 @@ public slots:
     void newFileSelected();
     void zoomIn();
     void zoomOut();
+    void changeZoom();
     void addNewTrack();
 
 private:
